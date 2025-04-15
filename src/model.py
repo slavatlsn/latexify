e9e277d4fa616c7ea16e6c613b69bf55494781a1
KoCaTkAA <135720809+KoCaTkAA@users.noreply.github.com>
2025-04-15 09:26:55 +0300

    segment model 1

--- src/model.py
+++ src/model.py
@@ -1,1 +1,11 @@
+from ultralytics import YOLO
+import os
 
+model = YOLO('yolov8n.pt')
+
+results = model.train(
+   data='dataset/dataset.yaml',
+   imgsz=640,
+   epochs=10,
+   batch=8,
+   name='yolov8n_custom')
--- src/pos_detect.py
+++ src/pos_detect.py
@@ -1,1 +1,9 @@
+import os
 
+a = set(map(lambda x: x[:x.rfind('.')], os.listdir(r'/src/runs/dataset/val/images')))
+b = set(map(lambda x: x[:x.rfind('.')], os.listdir(r'/src/runs/dataset/val/labels')))
+
+c = a - b
+print(c)
+#for el in c:
+#    os.remove(r'C:\Users/user/PycharmProjects/latexify/datasets/dataset/train/images/'+el+'.png')
