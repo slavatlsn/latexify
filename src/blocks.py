1dd65a1f5900397e28acfb64551523e36637028b
slavatlsn <93151297+slavatlsn@users.noreply.github.com>
2025-05-14 11:36:21 +0300

    Update blocks.py
    fixed residual block

--- src/blocks.py
+++ src/blocks.py
@@ -1,11 +1,19 @@
-import torch
+from torch import nn
 
-class Residual(torch.nn.Module):
+
+class Residual(nn.Module):
     def __init__(self, in_ch, out_ch, kernel, f_act):
         super().__init__()
         if kernel % 2:
-            self.main = torch.nn.Conv2d(in_channels = in_ch, out_channels = out_ch, kernel_size = kernel, padding = (kernel - 1)//2)
-            self.blank = torch.nn.Conv2d(in_channels = in_ch, out_channels = out_ch, kernel_size = 1) if in_ch != out_ch else torch.nn.Identity()
+            self.main = nn.Sequential(
+                nn.Conv2d(in_channels=in_ch, out_channels=out_ch, kernel_size=kernel, padding=(kernel - 1) // 2),
+                nn.BatchNorm2d(out_ch)
+            )
+            self.blank = nn.Sequential(
+                nn.Conv2d(in_channels=in_ch, out_channels=out_ch, kernel_size=1)
+                if in_ch != out_ch else nn.Identity(),
+                nn.BatchNorm2d(out_ch)
+            )
             self.activation = f_act
         else:
             raise RuntimeError('Kernel size must not be even')
