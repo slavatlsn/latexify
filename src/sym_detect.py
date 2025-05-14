0365cdbd75d3f0683b2dbc5beed067f1fb414819
slavatlsn <93151297+slavatlsn@users.noreply.github.com>
2025-05-14 11:11:06 +0300

    Update sym_detect.py

--- src/sym_detect.py
+++ src/sym_detect.py
@@ -1,4 +1,23 @@
 from torch import nn
+import torchvision.transforms as t
+import cv2
+
+transform = t.Compose([
+    t.ToPILImage(),
+    t.Resize((32, 32)),
+    t.Grayscale(num_output_channels=1),
+    t.ToTensor()
+])
+
+label_map = [
+    "(", ")", "a", "+", "b", "c", "d", "/", "e", "8", "=", "f", "5", "4",
+    "g", "h", "i", "j", "k", "l", "m", "n", "9", "o", "1", "p", "q", "r",
+    "s", "7", "6", "-", "t", "3", "2", "u", "v", "w", "x", "y", "z", "0",
+    "*", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+    "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+    r"\times", r"\div"
+]
+
 
 class SymNet(nn.Module):
     def __init__(self):
@@ -21,3 +40,15 @@ class SymNet(nn.Module):
 
     def forward(self, x):
         return self.network(x)
+
+
+classifier = SymNet()
+classifier.load_state_dict(load("SymNet.pth", map_location=device(device)))
+classifier.eval()
+
+def symbol():
+    gray_img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+    input_tensor = transform(gray_img).unsqueeze(0)
+    output = classifier(input_tensor)
+    class_id = argmax(output).item()
+    return label_map[class_id]
