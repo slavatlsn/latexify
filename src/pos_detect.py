09a5b8b074d305cd2f8f891344f88100ac0b9967
slavatlsn <93151297+slavatlsn@users.noreply.github.com>
2025-05-14 13:39:39 +0300

    Update pos_detect.py
    pos detection update

--- src/pos_detect.py
+++ src/pos_detect.py
@@ -1,9 +1,17 @@
-import os
+from ultralytics import YOLO
+import cv2
 
-a = set(map(lambda x: x[:x.rfind('.')], os.listdir(r'/src/runs/dataset/val/images')))
-b = set(map(lambda x: x[:x.rfind('.')], os.listdir(r'/src/runs/dataset/val/labels')))
+model_path = 'PosNet.pt'
+detector = YOLO(model_path)
 
-c = a - b
-print(c)
-#for el in c:
-#    os.remove(r'C:\Users/user/PycharmProjects/latexify/datasets/dataset/train/images/'+el+'.png')
+
+def image(path):
+    gray_img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+    return cv2.merge([gray_img, gray_img, gray_img])
+
+
+def data(path):
+    padding = 5
+    img = image(path)
+    result = [map(int, el) for el in detector.predict(img, conf=0.15, device='cpu')[0].boxes.xyxy]
+    return [cv2.threshold(img[el[1]-padding:el[3]+padding, el[0]-padding:el[2]+padding], 190, 255, cv2.THRESH_BINARY)[1] for el in map(list, result)]
