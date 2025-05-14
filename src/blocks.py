ccfa382464e9533032795f3d64d1c72c2982737e
slavatlsn <93151297+slavatlsn@users.noreply.github.com>
2025-05-14 11:04:15 +0300

    Update blocks.py
    remove unused

--- src/blocks.py
+++ src/blocks.py
@@ -12,23 +12,3 @@ class Residual(torch.nn.Module):
 
     def forward(self, x):
         return self.activation(self.main(x) + self.blank(x))
-
-class Inception(torch.nn.Module):
-    def __init__(self, in_ch, out_1, out_3, out_5):
-        super().__init__()
-        self.l1 = torch.nn.Conv2d(in_channels = in_ch, out_channels = out_1, kernel_size = 1)
-        self.l3 = torch.nn.Sequential(
-            torch.nn.Conv2d(in_channels = in_ch, out_channels = out_1, kernel_size = 1),
-            torch.nn.Conv2d(in_channels = out_1, out_channels = out_3, kernel_size = 3, padding = 1)
-        )
-        self.l5 = torch.nn.Sequential(
-            torch.nn.Conv2d(in_channels = in_ch, out_channels = out_1, kernel_size = 1),
-            torch.nn.Conv2d(in_channels = out_1, out_channels = out_5, kernel_size = 5, padding = 2)
-        )
-        self.pool = torch.nn.Sequential(
-            torch.nn.MaxPool2d(kernel_size = 3, stride = 1, padding = 1),
-            torch.nn.Conv2d(in_channels = in_ch, out_channels = out_1, kernel_size = 1)
-        )
-
-    def forward(self, x):
-        return torch.cat([self.l1(x), self.l3(x), self.l5(x), self.pool(x)], dim=1)
